ed560c6ebc3cb8d213712e73af429e922fc785f6
Kouji Fukushima <kf2821@gmail.com>
2019-06-20 17:37:49 +0900

    separeate process for loading graph

--- ssd_try.cpp
+++ ssd_try.cpp
@@ -9,26 +9,52 @@ const string graph_filename = "./export/trained_graph.pb";
 const string demo_dirname = "./SSD-Tensorflow/demo";
 const string checkpointPath = "./SSD-Tensorflow/checkpoints/ssd_300_vgg.ckpt";
 
+Status LoadGraph(string graph_filename, 
+                std::unique_ptr<GraphDef> &graph_def,
+                std::unique_ptr<Session>& session)
+{
+    Status load_graph_status =
+        ReadBinaryProto(Env::Default(), graph_filename, graph_def.get());
+    if (!load_graph_status.ok()) {
+    return errors::NotFound("Failed to load compute graph at '",
+                                        graph_filename, "'");
+    }
+    session.reset(NewSession(SessionOptions()));
+
+    Status session_create_status = session->Create(*graph_def);
+    if (!session_create_status.ok()) {
+    return session_create_status;
+    }
+    return Status::OK();
+}
+
 Tensor CreateTensorFromImage(string image_filename)
 {
-    
+
 }
+
 
 int main(){
 
     std::unique_ptr<Session> session;
     std::unique_ptr<GraphDef> graph(new GraphDef());
 
-    Status load_graph_status = ReadBinaryProto(Env::Default(), graph_filename, graph.get());
+    auto load_graph_status = LoadGraph(graph_filename, graph, session);
     if (!load_graph_status.ok()) {
-        throw std::runtime_error("Error reading graph definition from " + graph_filename + ": " + load_graph_status.ToString());
+        LOG(ERROR) << load_graph_status.error_message();
+        return -1;
     }
 
-    session.reset(NewSession(SessionOptions()));
-    Status session_create_status = (session)->Create(*graph);
-    if (!session_create_status.ok()) {
-        throw std::runtime_error("Error reading load graph : " + load_graph_status.ToString());
-    }
+    // Status load_graph_status = ReadBinaryProto(Env::Default(), graph_filename, graph.get());
+    // if (!load_graph_status.ok()) {
+    //     throw std::runtime_error("Error reading graph definition from " + graph_filename + ": " + load_graph_status.ToString());
+    // }
+
+    // session.reset(NewSession(SessionOptions()));
+    // Status session_create_status = (session)->Create(*graph);
+    // if (!session_create_status.ok()) {
+    //     throw std::runtime_error("Error reading load graph : " + load_graph_status.ToString());
+    // }
     
     /* If Graph is freeze graph, there is no need to refer checkpoint */
     Tensor checkpointPathTensor(DT_STRING, TensorShape());
