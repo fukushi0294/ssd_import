a183aa9d77140a5d29514690da150272576a2202
Kouji Fukushima <kf2821@gmail.com>
2019-06-20 13:33:18 +0900

    Initial commit!

---
+++ ssd_try.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <tensorflow/core/platform/init_main.h>
+#include <tensorflow/core/public/session.h>
+#include <tensorflow/core/util/command_line_flags.h>
+
+
+
+int main(){
+    tensorflow::string model_file = 
+}
